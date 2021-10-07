a0328c43d0c0fc5d1169b32a067ab19da32bb61b
OblivionLi <liviuandre.dev@gmail.com>
2021-10-07 16:11:49 +0300

    Fix api/web.php routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,38 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\ChapterController;
+use App\Http\Controllers\Api\GenreController;
+use App\Http\Controllers\Api\PermissionController;
+use App\Http\Controllers\Api\ReviewBookController;
+use App\Http\Controllers\Api\ReviewChapterController;
+use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Public Routes
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+# Auth
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+# Logged in user routes
+Route::group(['middleware' => 'auth:api'], function () {
+    # Public
+    Route::apiResource('book-reviews', ReviewBookController::class);
+    Route::apiResource('chapter-reviews', ReviewChapterController::class);
+    Route::apiResource('chapters', ChapterController::class);
+    Route::apiResource('genres', GenreController::class);
+    Route::apiResource('books', BookController::class);
+    Route::apiResource('roles', RoleController::class);
+    Route::apiResource('permissions', PermissionController::class);
+    Route::apiResource('users', UserController::class)->except(['store']);
+
+    # Private 
+    Route::group(['middleware' => 'isAdmin'], function () {
+        # Role Routes
+    });
 });
+// Private Routes
