6b1df414d3a487a3bb389c58c4b11646689cd35a
OblivionLi <liviuandre.dev@gmail.com>
2021-10-07 16:12:19 +0300

    Initial Commit + Add Vue.js

---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,11 @@
+<template>
+   <p>text from test component</p>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/routes.js
@@ -0,0 +1,10 @@
+
+import TestComponent from './components/TestComponent';
+
+export const routes = [
+    {
+        name: 'test',
+        path: '/test',
+        component: TestComponent
+    },
+];
