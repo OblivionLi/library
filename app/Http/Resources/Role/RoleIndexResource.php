a103e46345051d5dd4c9441409d860f5db6622ea
OblivionLi <liviuandre.dev@gmail.com>
2021-10-07 16:04:57 +0300

    Add Role's resources/requests classes

---
+++ app/Http/Resources/Role/RoleIndexResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Role;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RoleIndexResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name'          => $this->name,
+            'is_admin'      => $this->is_admin,
+            'users'         => $this->users->pluck('name'),
+            'permissions'   => $this->permissions->pluck('name'),
+
+            'created_at'    => $this->created_at,
+            'updated_at'    => $this->updated_at,
+        ];
+    }
+}
