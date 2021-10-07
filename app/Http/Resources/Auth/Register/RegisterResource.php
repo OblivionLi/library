f6ceda1ed6c732c8587956a5d781758c82aec788
OblivionLi <liviuandre.dev@gmail.com>
2021-10-07 16:05:16 +0300

    Add Auth controller register/login resources

---
+++ app/Http/Resources/Auth/Register/RegisterResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\Auth\Register;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RegisterResource extends JsonResource
+{
+    protected $token;
+
+    public function __construct($resource, $token)
+    {
+        parent::__construct($resource);
+        $this->token = $token;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'message'       => 'User register success',
+            'id'            => $this->id,
+            'name'          => $this->name,
+            'email'         => $this->email,
+            'role'          => $this->roles->pluck('name'),
+            'is_admin'      => $this->roles->pluck('is_admin'),
+            'access_token'  => $this->token,
+        ];
+    }
+}
