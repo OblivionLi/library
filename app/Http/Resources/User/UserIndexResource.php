e44e9748932defe0544c660982f0ece64d6d0d83
OblivionLi <liviuandre.dev@gmail.com>
2021-10-07 16:01:22 +0300

    Add User controller requests and resources

---
+++ app/Http/Resources/User/UserIndexResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\User;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserIndexResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name'              => $this->name,
+            'email'             => $this->email,
+            'created_at'        => $this->created_at,
+            'updated_at'        => $this->updated_at,
+
+            'books'             => $this->books,
+            'bookReviews'       => $this->bookReviews,
+            'chapterReviews'    => $this->chapterReviews,
+            'chapters'          => $this->chapters,
+            'roles'             => $this->roles->pluck('name'),
+        ];
+    }
+}
