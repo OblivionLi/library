e4529af48d4acb2ee4d8fd986e1b2017ada0c139
OblivionLi <liviuandre.dev@gmail.com>
2021-10-14 17:41:46 +0300

    Add routes - test purposes

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,19 @@ use Illuminate\Support\Facades\Route;
 # Auth
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
+Route::patch('reset-password/{email}', [AuthController::class, 'resetPassword']);
+Route::get('reset-password/{token}', [AuthController::class, 'getToken']);
+Route::patch('update-credentials/{id}', [AuthController::class, 'update']);
 
+Route::apiResource('users', UserController::class)->except(['store']);
 # Logged in user routes
 Route::group(['middleware' => 'auth:api'], function () {
     # Public
+
+    // User Routes
+    Route::get('logout', [AuthController::class, 'logout']);
+
     Route::apiResource('book-reviews', ReviewBookController::class);
     Route::apiResource('chapter-reviews', ReviewChapterController::class);
     Route::apiResource('chapters', ChapterController::class);
@@ -28,7 +37,6 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResource('books', BookController::class);
     Route::apiResource('roles', RoleController::class);
     Route::apiResource('permissions', PermissionController::class);
-    Route::apiResource('users', UserController::class)->except(['store']);
 
     # Private 
     Route::group(['middleware' => 'isAdmin'], function () {
