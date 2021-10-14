f807f8a9b3585980bb56224b7da5862bd71dcc3d
OblivionLi <liviuandre.dev@gmail.com>
2021-10-14 17:35:12 +0300

    initial commit

---
+++ resources/js/components/book/BookComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <p>Hello from book_component</p>
+        <p>with login</p>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    created() {
+        // const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+        // axios.defaults.headers.common["Authorization"] =
+        //     "Bearer " + localStorage.getItem(`${userInfo.access_token}`);
+    }
+};
+</script>
---
+++ resources/js/components/homescreen/HomescreenComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container"> 
+        <p>Hello from homescreen_component</p>
+
+        
+    </div>
+</template>
+ 
+<script>
+    export default {}
+</script>
