d7b7b48fc3cdb8e494585f26aced699a24f13c64
OblivionLi <liviuandre.dev@gmail.com>
2021-10-14 17:09:57 +0300

    Add functions to AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,14 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\ForgotPassword\ForgotPasswordRequest;
 use App\Http\Requests\Auth\Login\LoginRequest;
 use App\Http\Requests\Auth\Register\RegisterRequest;
+use App\Http\Requests\Auth\ResetPassword\ResetPasswordRequest;
+use App\Http\Requests\Auth\UpdateUser\UpdateUserRequest;
 use App\Http\Resources\Auth\Login\LoginResource;
 use App\Http\Resources\Auth\Register\RegisterResource;
+use App\Mail\ForgotPassword;
 use App\Models\Role;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -89,5 +97,111 @@ class AuthController extends Controller
         // return error if user doesnt exist 
         $response = ['message' => 'Invalid user credentials..'];
         return response()->json($response);
+    }
+
+    // logout user
+    public function logout(Request $request) {
+        // revoke user token
+        $request->user()->token()->revoke();
+
+        // return success message
+        $response = ['message' => "You have been logged out"];
+        return response()->json($response);
+    }
+
+    // forgot-password
+    public function forgotPassword(ForgotPasswordRequest $request)
+    {
+        // create a token with 60 random characters
+        $token = Str::random(60);
+
+        // insert data into password_resets table
+        // the table takes an email, a token and a date when the record was inserted
+        DB::table('password_resets')->insert([
+            [
+                'email'         => $request->email,
+                'token'         => $token,
+                'created_at'    => Carbon::now()
+            ]
+        ]);
+
+        // get user where the email is = with the email from request
+        $user = User::where('email', $request->email)->first();
+
+        // prepare an array with the user found and the randomly generated token
+        // the data will be used in the sent email
+        $data = [
+            'user'  => $user,
+            'token' => $token
+        ];
+
+        // sent the prepared data to the email inside the request
+        Mail::to($request->email)->send(new ForgotPassword($data));
+
+        // return success message
+        $response = ['message' => 'Email sent with success'];
+        return response()->json($response, 200);
+    }
+
+    // reset password
+    public function resetPassword(ResetPasswordRequest $request, $email)
+    {
+        // get user by email
+        $user = User::where('email', $email)->first();
+
+        // check if user exist
+        if ($user) {
+            // if password exist in request then hash it and add it to $user obj
+            $request->password && $user->password = Hash::make($request->password);
+
+            // save user to db with the newly hashed password
+            $user->save();
+
+            // delete token from password_resets table
+            DB::table('password_resets')->where('email', $email)->delete();
+        } else {
+            // if user doesnt exist then throw error response
+            $response = ['message' => 'User does not exist..'];
+            return response()->json($response, 404);
+        }
+
+        // return success message
+        $response = ['message' => 'User password changed with success'];
+        return response()->json($user, 200);
+    }
+
+    // get token from password_resets table
+    public function getToken($token)
+    {
+        // return a json response with the token
+        return response()->json(DB::select('select *  from password_resets where token = :token', ['token' => $token]));
+    }
+
+    // update user credentials
+    public function update(UpdateUserRequest $request, $id)
+    {
+        // get user by id
+        $user = User::find($id);
+
+        // if user exist
+        if ($user) {
+            // insert data into user object
+            $user->name = $request->name;
+            $user->email = $request->email;
+
+            // check if request has password and hash it
+            $request->password && Hash::make($request->password);
+
+            // save user obj with the new data
+            $user->save();
+        } else {
+            // return error message
+            $response = ['message' => 'User does not exist..'];
+            return response()->json($response, 404);
+        }
+
+        // return success message
+        $response = ['message' => 'User Credentials have been updated successfully'];
+        return response()->json($user, 200);
     }
 }
