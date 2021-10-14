537abadbdb59fb977db9b96f107abff461588d2b
OblivionLi <liviuandre.dev@gmail.com>
2021-10-14 17:12:19 +0300

    Add forgot pass mail class that sends data to blade component

---
+++ app/Mail/ForgotPassword.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ForgotPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $token;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->user     = $data['user'];
+        $this->token    = $data['token'];
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('email.forgotPassword');
+    }
+}
