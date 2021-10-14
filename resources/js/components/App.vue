aef7e61b7eaf52f36f7505e6565a095fe04c92cc
OblivionLi <liviuandre.dev@gmail.com>
2021-10-14 17:18:19 +0300

    Move App.vue to different directory and reformat its content in comps

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <MainNavbarComponent />
+        <router-view />
+    </div>
+</template>
+
+<script>
+import MainNavbarComponent from "./navigation/MainNavbarComponent.vue";
+
+export default {
+    components: {
+        MainNavbarComponent
+    }
+};
+</script>
